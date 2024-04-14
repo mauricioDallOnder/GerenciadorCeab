e6e736947a631b475b5ffc216fd44dde12965d85
mauricioDallOnder <londer11@icloud.com>
2024-04-14 01:07:23 -0300

    fix issues

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -1,40 +1,103 @@
-import { AppBar, Toolbar, Button, Box,makeStyles } from '@mui/material';
-import React from 'react';
+'use client'
+import * as React from 'react';
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Avatar,
+  Drawer,
+  Button,
+  Typography,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import Link from 'next/link';
 
-function MyAppBar() {
-  const toolbarStyles = {
-    display: 'flex',
-    justifyContent: 'space-between',
+
+const pages = [
+  { title: "Cadastro", link: "/FormRegistration" },
+  { title: "Atualização de dados", link: "/FormUpdate" },
+  { title: "Lista de Associados", link: "/ListOfUsers" },
+  { title: "Relatórios", link: "/Reports" },
+];
+//teste
+//link atualizado
+export default function ResponsiveAppBar() {
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
   };
-  
-  const linkStyles = {
-    textDecoration: 'none',
-    color:"white",
-    margin: '0 16px',
-    fontWeight: "700",
-    fontFamily:'Roboto',
-  };
-  return (
-    <AppBar position="fixed"  style={{ top: 0, height: '64px', zIndex: 1,background: "#202932"}}>
-    <Toolbar sx={toolbarStyles}>
-    <Link style={linkStyles} href="/FormRegistration">
-        Formulário de Cadastro
-      </Link>
-      <Link style={linkStyles} href="/FormUpdate">
-       Atualização Cadastral
-      </Link>
-      
-      <Link style={linkStyles} href="/ListOfUsers">
-        Lista de Usuários
-      </Link>
-      <Link style={linkStyles} href="/Reports">
-       Relatórios
-      </Link>
+
+  const renderList = (items: typeof pages, direction: 'column' | 'row' = 'row') => (
+    <List sx={{ display: 'flex', flexDirection: direction,alignItems:'center',gap:'10px' }}>
+      {items.map((item) => (
+        <Link href={item.link} passHref key={item.title}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary={item.title} />
+            </ListItemButton>
+            
+          </ListItem>
+        </Link>
+      ))}
      
-    </Toolbar>
-  </AppBar>
+    </List>
+  );
+
+  const links = renderList(pages);
+  const drawerLinks = renderList(pages, 'column');
+
+  return (
+    <AppBar position="static" color='primary'>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%',alignItems:'center' }}>
+            <Link href={"/"}>
+              <Avatar
+                sx={{ width: 80, height: 80, bgcolor: 'white',marginBottom:'5px',marginTop:'5px' }}
+              >
+                <img src="https://firebasestorage.googleapis.com/v0/b/chat-dos-otarios.appspot.com/o/ceab%2Falunos.6466ab2a87c256.49461801.jpg?alt=media&token=42b3312e-02ba-4379-8d1a-8ae0fc96b20a" alt="" />
+              </Avatar>
+            </Link>
+            
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+              {links}
+            </Box>
+          </Box>
+        </Toolbar>
+      </Container>
+      <Drawer
+        anchor='right'
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+      >
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="close drawer"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2 }}
+        >
+          <CloseIcon />
+        </IconButton>
+        {drawerLinks}
+      </Drawer>
+    </AppBar>
   );
 }
-
-export default MyAppBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,134 @@
+import { Container, Grid, Card, CardMedia, Typography } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   
   return (
     <main >
      
-       <div>teste</div>
+     <Container>
+            <Grid container spacing={12} style={{marginTop: "15%"}}>
+            <Grid item xs={12} sm={6} md={3}>
+                <Link href="/FormRegistration"> {/* Adiciona o link para o Card */}
+                 
+                <Card sx={{
+                  textAlign: 'center',
+                  backgroundColor: '#f7f7f7', // Cor de fundo suave
+                  boxShadow: 3, // Sombra sutil
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  '&:hover': {
+                   
+                    cursor: 'pointer'
+                  }
+                }}>
+                      <CardMedia 
+                        component="img"
+                        height="fit-content"
+                        width='fit-content'
+                        image='https://firebasestorage.googleapis.com/v0/b/chat-dos-otarios.appspot.com/o/ceab%2Fresume.png?alt=media&token=6edbaf5e-7e2b-40a0-a3e2-9811e3631a75'
+                        alt=""
+                      />
+                      <Typography color='#FFFF' mt={3}  variant="h5">Cadastro</Typography>
+                     
+                    </Card>
+                 
+                </Link>
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Link href="/FormUpdate"> {/* Adiciona o link para o Card */}
+                 
+                <Card sx={{
+                  textAlign: 'center',
+                  backgroundColor: '#f7f7f7', // Cor de fundo suave
+                  boxShadow: 3, // Sombra sutil
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  '&:hover': {
+                  
+                    cursor: 'pointer'
+                  }
+                }}>
+                      <CardMedia 
+                        component="img"
+                        height="fit-content"
+                        width='fit-content'
+                        image='https://firebasestorage.googleapis.com/v0/b/chat-dos-otarios.appspot.com/o/ceab%2Fupdate.png?alt=media&token=bc1e2318-041e-4154-8f27-8d0a9faa80ae'
+                        alt=""
+                      />
+                      <Typography color='#FFFF' mt={3}  variant="h5">Atualização Cadastral</Typography>
+                      <Typography color='#FFFF' mt={27}  variant="h5"></Typography>
+                    </Card>
+                 
+                </Link>
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Link href="/ListOfUsers"> {/* Adiciona o link para o Card */}
+                 
+                <Card sx={{
+                  textAlign: 'center',
+                  backgroundColor: '#f7f7f7', // Cor de fundo suave
+                  boxShadow: 3, // Sombra sutil
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  '&:hover': {
+                   
+                    cursor: 'pointer'
+                  }
+                }}>
+                      <CardMedia 
+                        component="img"
+                        height="fit-content"
+                        width='fit-content'
+                        image='https://firebasestorage.googleapis.com/v0/b/chat-dos-otarios.appspot.com/o/ceab%2Fchecklist.png?alt=media&token=95d85403-ac4e-49a2-a546-cd7615c6ff50'
+                        alt=""
+                      />
+                      <Typography color='#FFFF' mt={3}  variant="h5">Lista de associados</Typography>
+                      <Typography color='#FFFF' mt={27}  variant="h5"></Typography>
+                    </Card>
+                
+                </Link>
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <Link href="/Reports">
+                  
+                <Card sx={{
+                  textAlign: 'center',
+                  backgroundColor: '#f7f7f7', // Cor de fundo suave
+                  boxShadow: 3, // Sombra sutil
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  '&:hover': {
+                   
+                    cursor: 'pointer'
+                  }
+                }}>
+                      <CardMedia 
+                        component="img"
+                        height="fit-content"
+                        width='fit-content'
+                        image='https://firebasestorage.googleapis.com/v0/b/chat-dos-otarios.appspot.com/o/ceab%2Fclock.png?alt=media&token=061a08d4-b13c-41bb-952b-a1a53b0f9ca3'
+                        alt=""
+                      />
+                      <Typography color='#FFFF' mt={3}  variant="h5">Relatórios</Typography>
+                      <Typography color='#FFFF' mt={27}  variant="h5"></Typography>
+                    </Card>
+                  
+                </Link>
+              </Grid>
+
+             
+            </Grid>
+          </Container>
      
     </main>
   );
